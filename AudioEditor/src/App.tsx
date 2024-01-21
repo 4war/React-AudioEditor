c99c63793b434342ec47064132e029c8fad55792
PhoWar <fedro_777@mail.ru>
2024-01-21 10:12:15 +0400

    темы

--- AudioEditor/src/App.tsx
+++ AudioEditor/src/App.tsx
@@ -1,24 +1,26 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './index.scss';
+import {  Route, Routes } from 'react-router-dom';
+import './styles/index.scss';
 import { Link } from 'react-router-dom';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { Suspense } from 'react';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <BrowserRouter>
-            <div className="app">
-                <Link to={''}>Главная</Link>
-                <Link to={'/about'}>О сайте</Link>
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Routes>
-                        <Route path={'/about'} element={<AboutPageAsync />} />
-                        <Route path={'/'} element={<MainPageAsync />} />{' '}
-                    </Routes>
-                </Suspense>
-            </div>
-        </BrowserRouter>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle</button>
+            <Link to={''}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />{' '}
+                </Routes>
+            </Suspense>
+        </div>
     );
 };
 
