55e889430b114f0912b3f9ce71e845dd57e0b610
PhoWar <fedro_777@mail.ru>
2024-04-06 10:59:52 +0400

    fixed gitignore

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,64 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import webpack from 'webpack';
+import { BuildOptions } from './types/config';
+
+export function buildLoaders (options: BuildOptions): webpack.RuleSetRule[] {
+    const svgLoader = {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack']
+    };
+
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        'i18next-extract',
+                        { locales: ['ru', 'en'], keyAsDefaultValue: true }
+                    ]
+                ]
+            }
+        }
+    };
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+            {
+                loader: 'file-loader'
+            }
+        ]
+    };
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        auto: (resPath: string) => resPath.includes('.module.'),
+                        localIdentName: options.isDev
+                            ? '[path][name]__[local]--[hash:base64:4]'
+                            : '[hash:base64:8]'
+                    }
+                }
+            },
+            'sass-loader'
+        ]
+    };
+
+    const tsLodaer: webpack.RuleSetRule = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+    };
+
+    return [fileLoader, babelLoader, svgLoader, tsLodaer, cssLoader];
+}
