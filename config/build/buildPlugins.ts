85b53afa60375404b2a894b1ad1629c5332828a5
PhoWar <fedro_777@mail.ru>
2024-04-07 09:45:00 +0400

    fixed git acitons

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -7,22 +7,28 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 export function buildPlugins(
     options: BuildOptions
 ): webpack.WebpackPluginInstance[] {
-    return [
+    const plugins = [
         new webpack.ProgressPlugin(),
         new HtmlWebpackPlugin({ template: options.paths.html }),
         new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css'
         }),
-        new webpack.DefinePlugin({
-            __IS_DEV__: JSON.stringify(options.isDev)
-        }),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false
-        })
-
-        // "hot: true" automatically applies HMR plugin, you don't have to add it manually to your webpack configuration.
-        // new ReactRefreshWebpackPlugin(),
-        // new webpack.HotModuleReplacementPlugin(),
+        ...(options.isDev
+            ? [
+                new webpack.DefinePlugin({
+                    __IS_DEV__: JSON.stringify(options.isDev)
+                })
+            ]
+            : []),
+        ...(options.isDev
+            ? [
+                new BundleAnalyzerPlugin({
+                    openAnalyzer: false
+                })
+            ]
+            : [])
     ];
+
+    return plugins;
 }
